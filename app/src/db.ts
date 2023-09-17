6ea91e86a03d3590ad7a1a569516f46c088ffe1c
berke <berke.ahlatci@gmail.com>
2023-09-17 16:42:08 +0300

    refactor db access

---
+++ app/src/db.ts
@@ -0,0 +1,11 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('../codeway-task-33a0d-firebase-adminsdk-88mu9-d9ad6f78c3.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://codeway-task-33a0d.firebaseio.com'
+});
+
+const db = admin.firestore();
+
+export default db;
