e4cc762fe20979d7810fbd099723a7dc97a98874
berke <berke.ahlatci@gmail.com>
2023-09-17 16:45:03 +0300

    refactor utils

--- app/src/routes.ts
+++ app/src/routes.ts
@@ -1,6 +1,7 @@
 
 import { Router } from 'express';
 import { Configuration } from './models/configuration.model';
+import { toHumanReadableFormat, camelToSnake } from './utils';
 import db from './db'; 
 
 const router = Router();
@@ -35,20 +36,6 @@ async function getConfigurations() {
         configurationArray.push(configuration);
     });
     return configurationArray;
-}
-
-function camelToSnake(str: string) {
-    return str.replace(/[A-Z]/g, (c: string) => '_' + c.toLowerCase());
-}
-
-function toHumanReadableFormat(date: Date): string {
-    const day = date.getDate().toString().padStart(2, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // +1 because months are 0-based
-    const year = date.getFullYear();
-    const hours = date.getHours().toString().padStart(2, '0');
-    const minutes = date.getMinutes().toString().padStart(2, '0');
-
-    return `${day}/${month}/${year} ${hours}:${minutes}`;
 }
 
 export default router;
