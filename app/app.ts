af0ab4728125fd7b5d4aa9b89a183609db8095f9
berke <berke.ahlatci@gmail.com>
2023-09-18 13:55:33 +0300

    add cors to backend

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 import routes from './src/routes';
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3000;
 
 app.use('/', routes)
