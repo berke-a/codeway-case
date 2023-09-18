834c8fd1718a271e24aa573c0b3af5a8a6cdb1c1
berke <berke.ahlatci@gmail.com>
2023-09-18 14:27:39 +0300

    add logo

--- app-ui/src/App.vue
+++ app-ui/src/App.vue
@@ -1,4 +1,7 @@
 <template>
+  <header>
+    <img src="./assets/icon.png" alt="App Icon" class="header-icon">
+  </header>
   <ConfigurationsTable/>
 </template>
 
@@ -15,13 +18,21 @@ export default {
 
 <style>
 body { margin: 0 !important; }
+header {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+.header-icon {
+  width: 3vw;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  padding-bottom: 200px;
+  padding: 20px;
   background-image: linear-gradient(#1E1E2E,#1E1E25)
 }
 </style>
