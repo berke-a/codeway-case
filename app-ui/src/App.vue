f879eae4bb9e9e2f2600e8b41f6c831456bc87f8
berke <berke.ahlatci@gmail.com>
2023-09-18 15:05:46 +0300

    update background

--- app-ui/src/App.vue
+++ app-ui/src/App.vue
@@ -30,9 +30,11 @@ header {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   padding: 20px;
-  background-image: linear-gradient(#1E1E2E,#1E1E25)
+  background-image: linear-gradient(#1E1E2E,#1E1E25);
+  background-size: cover;
+  width: 100vw;
+  height: 100vh;
 }
 </style>
