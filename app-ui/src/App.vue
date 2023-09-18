a484e21ca0c0c5747029329ffbade77960ad63ad
berke <berke.ahlatci@gmail.com>
2023-09-18 14:19:36 +0300

    style app

--- app-ui/src/App.vue
+++ app-ui/src/App.vue
@@ -18,12 +18,14 @@ export default {
 </script>
 
 <style>
+body { margin: 0 !important; }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding-bottom: 200px;
+  background-image: linear-gradient(#1E1E2E,#1E1E25)
 }
 </style>
--- app-ui/src/components/ConfigurationsTable.vue
+++ app-ui/src/components/ConfigurationsTable.vue
@@ -1,56 +1,85 @@
 <template>
     <div>
-      <table v-if="configurations.length">
-        <thead>
-          <tr>
-            <th>Parameter Key</th>
-            <th>Value</th>
-            <th>Type</th>
-            <th>Description</th>
-            <th>Create Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="config in configurations" :key="config.id">
-            <td>{{ config.parameterKey }}</td>
-            <td>{{ config.value }}</td>
-            <td>{{ config.type }}</td>
-            <td>{{ config.description }}</td>
-            <td>{{ config.createDate }}</td>
-          </tr>
-        </tbody>
-      </table>
-      <p v-else>Loading configurations...</p>
+        <table v-if="configurations.length">
+            <thead>
+                <tr>
+                    <th>Parameter Key</th>
+                    <th>Value</th>
+                    <th>Type</th>
+                    <th>Description</th>
+                    <th>Create Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="config in configurations" :key="config.id">
+                    <td>{{ config.parameterKey }}</td>
+                    <td>{{ config.value }}</td>
+                    <td>{{ config.type }}</td>
+                    <td>{{ config.description }}</td>
+                    <td>{{ config.createDate }}</td>
+                </tr>
+            </tbody>
+        </table>
+        <p v-else>Loading configurations...</p>
     </div>
-  </template>
+</template>
   
-  <script>
-  import axios from 'axios';
-  
-  export default {
+<script>
+import axios from 'axios';
+
+export default {
     name: 'ConfigurationsTable',
     data() {
-      return {
-        configurations: []
-      };
+        return {
+            configurations: []
+        };
     },
     methods: {
-      async fetchConfigurations() {
-        try {
-          const response = await axios.get('http://localhost:3000/');
-          console.log(response);
-          this.configurations = response.data.data;
-        } catch (error) {
-          console.error("An error occurred:", error);
+        async fetchConfigurations() {
+            try {
+                const response = await axios.get('http://localhost:3000/');
+                console.log(response);
+                this.configurations = response.data.data;
+            } catch (error) {
+                console.error("An error occurred:", error);
+            }
         }
-      }
     },
     mounted() {
-      this.fetchConfigurations();
+        this.fetchConfigurations();
     }
-  };
-  </script>
+};
+</script>
   
-  <style scoped>
-  </style>
+<style scoped>
+thead {
+    color: #778CA3;
+    font-weight: bold;
+}
+
+tbody {
+    color: #FFFF;
+}
+
+th,
+td {
+    padding: 8px 12px;
+    /* 8px top/bottom, 12px left/right */
+}
+
+table {
+    border-collapse: collapse;
+    width: 100%;
+    margin: 20px 0;
+    /* 20px top/bottom, 0 left/right */
+}
+
+@media only screen and (max-width: 600px) {
+    table {
+        display: block;
+        overflow-x: auto;
+        white-space: nowrap;
+    }
+}
+</style>
   
