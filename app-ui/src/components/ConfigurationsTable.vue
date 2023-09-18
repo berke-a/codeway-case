7183c349f657b272dd4af1d457fb0c8dc30c262f
berke <berke.ahlatci@gmail.com>
2023-09-18 15:36:44 +0300

    add buttons to edit and delete

--- app-ui/src/components/ConfigurationsTable.vue
+++ app-ui/src/components/ConfigurationsTable.vue
@@ -9,6 +9,12 @@
             <tbody>
                 <tr v-for="config in configurations" :key="config.parameterKey">
                     <td v-for="(value, key) in config" :key="key">{{ value }}</td>
+                    <td>
+                        <button class="edit" @click="editConfig(config)">Edit</button>
+                    </td>
+                    <td>
+                        <button class="delete" @click="deleteConfig(config)">Delete</button>
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -37,6 +43,12 @@ export default {
             } catch (error) {
                 console.error("An error occurred:", error);
             }
+        },
+        editConfig(config) {
+            console.log("Editing configuration:", config);
+        },
+        deleteConfig(config) {
+            console.log("Deleting configuration:", config);
         }
     },
     mounted() {
@@ -46,10 +58,17 @@ export default {
 </script>
   
 <style scoped>
+table {
+    border-collapse: collapse;
+    width: 100%;
+    margin: 20px 0;
+    text-align: left;
+}
+
 thead {
     color: #778CA3;
     font-size: larger;
-    
+
 }
 
 tbody {
@@ -62,11 +81,20 @@ td {
     padding: 8px 12px 8px 0px;
 }
 
-table {
-    border-collapse: collapse;
-    width: 100%;
-    margin: 20px 0;
-    text-align: left;
+button {
+    padding: 5px 10px;
+    border-radius: 5px;
+    border: none;
+    color: #FFFF;
+    font-weight: bold;
+}
+
+.edit {
+    background-image: linear-gradient(45deg, #3161F5, #2089F8) ;
+}
+
+.delete {
+    background-image: linear-gradient(45deg, #EF363B, #FB5681) ;
 }
 
 @media only screen and (max-width: 600px) {
