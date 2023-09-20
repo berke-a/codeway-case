f98e8a930c8e4f51c21d24115b70b6dcc2b80e09
berke <berke.ahlatci@gmail.com>
2023-09-20 23:08:29 +0300

    remove width

--- app-ui/src/App.vue
+++ app-ui/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-view/>
+  <router-view />
 </template>
 
 <script>
@@ -12,16 +12,18 @@ export default {
 </script>
 
 <style>
-body { margin: 0 !important; }
+body {
+  margin: 0 !important;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   padding: 20px;
-  background-image: linear-gradient(#1E1E2E,#1E1E25);
+  background-image: linear-gradient(#1E1E2E, #1E1E25);
   background-size: cover;
-  width: 100vw;
   height: 100vh;
 }
 </style>
