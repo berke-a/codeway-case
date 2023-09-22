03a05697a7c1c7b2cffb921aa0ea0b34a397a03f
berke <berke.ahlatci@gmail.com>
2023-09-22 19:51:28 +0300

    placeholder in mobile add config

--- app-ui/src/components/ConfigurationsMobileComponent.vue
+++ app-ui/src/components/ConfigurationsMobileComponent.vue
@@ -35,7 +35,13 @@
                     <option value="float">float</option>
                     <option value="string">string</option>
                 </select>
-                <input v-else-if="key !== 'createDate'" v-model="newConfig[key]" type="text" />
+                <input v-else-if="key === 'parameterKey'" placeholder="New Parameter (snake_case)" v-model="newConfig[key]"
+                    type="text" />
+                <input v-else-if="key === 'value'" placeholder="Value" v-model="newConfig[key]" type="text" />
+                <input v-else-if="key === 'description'" placeholder="New Description" v-model="newConfig[key]"
+                    type="text" />
+                <span v-else-if="key === 'createDate'" class="mobile-value">{{ currentDate }}</span>
+                <span class="mobile-value" v-else>{{ value }}</span>
             </div>
             <button @click="addConfig(newConfig)" class=" button-add">Add</button>
         </div>
@@ -47,6 +53,7 @@
 export default {
     data() {
         return {
+            currentDate: this.toHumanReadableFormat(new Date()),
             chosenConfig: null,
             newConfig: {
                 parameterKey: '',
@@ -64,6 +71,18 @@ export default {
         deleteConfig: Function,
         addConfig: Function,
     },
+    methods: {
+        toHumanReadableFormat(date) {
+            const day = date.getDate().toString().padStart(2, '0');
+            const month = (date.getMonth() + 1).toString().padStart(2, '0');
+            const year = date.getFullYear();
+            const hours = date.getHours().toString().padStart(2, '0');
+            const minutes = date.getMinutes().toString().padStart(2, '0');
+
+            return `${day}/${month}/${year} ${hours}:${minutes}`;
+        }
+
+    }
 };
 </script>
 
