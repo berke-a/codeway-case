256698896904faf913b47b40e858312f16b4f9af
berke <berke.ahlatci@gmail.com>
2023-09-23 16:51:12 +0300

    refactor mobile body

--- app-ui/src/components/ConfigurationsMobileComponent.vue
+++ app-ui/src/components/ConfigurationsMobileComponent.vue
@@ -1,51 +1,17 @@
 <template>
     <div>
-        <div class="mobile-config" v-for="config in configurations" :key="config.parameterKey">
-            <div class="config-field" v-for="(value, key) in config" :key="key">
-                <div v-if="chosenConfig === config.parameterKey && !isDeleteButtonClicked">
-                    <span class="mobile-key">{{ key }}:</span>
-                    <select v-if="key === 'type'" v-model="config[key]">
-                        <option value="int">int</option>
-                        <option value="float">float</option>
-                        <option value="string">string</option>
-                    </select>
-                    <input v-else-if="key !== 'createDate' && key !== 'parameterKey'" v-model="config[key]" type="text" />
-                    <span class="mobile-value" v-else>{{ value }}</span>
-                </div>
-                <div v-else>
-                    <span class="mobile-key">{{ key }}:</span> <span class="mobile-value">{{ value }}</span>
-                </div>
-            </div>
-
-            <div>
-                <div v-if="chosenConfig === config.parameterKey" class="config-buttons">
-                    <button v-if="!isDeleteButtonClicked" class="button-done"
-                        @click="editConfig(config).then(chosenConfig = null)">&#10003;</button>
-                    <button v-if="!isDeleteButtonClicked" style="z-index: -1"></button>
-
-                    <button v-if="isDeleteButtonClicked" style="z-index: -1"></button>
-                    <button v-if="isDeleteButtonClicked" class="button-done"
-                        @click="deleteConfig(config).then(chosenConfig = null)">&#10003;</button>
-
-                </div>
-                <div v-else class="config-buttons">
-                    <button class="button-edit"
-                        @click="chosenConfig = config.parameterKey; isDeleteButtonClicked = false">Edit</button>
-                    <button class="button-delete" @click="chosenConfig = config.parameterKey; isDeleteButtonClicked = true">
-                        Delete</button>
-                </div>
-
-            </div>
-        </div>
+        <ConfigurationsMobileBody :configurations="configurations" :editConfig="editConfig" :deleteConfig="deleteConfig" />
         <ConfigurationMobileAdd :addConfig="addConfig" />
     </div>
 </template>
 
 <script>
 import ConfigurationMobileAdd from "./add/ConfigurationMobileAdd.vue";
+import ConfigurationsMobileBody from "./ConfigurationsMobileBody.vue";
 
 export default {
     components: {
+        ConfigurationsMobileBody,
         ConfigurationMobileAdd,
     },
     data() {
