1eed47a925752d5e48268214df0bde77222aa5f8
berke <berke.ahlatci@gmail.com>
2023-09-23 16:32:38 +0300

    fix edit/delete

--- app-ui/src/components/tables/ConfigurationTableBody.vue
+++ app-ui/src/components/tables/ConfigurationTableBody.vue
@@ -1,14 +1,16 @@
 <template>
     <tbody>
-        <tr v-for="config in configurations" :key="config.parameterKey">
+        <tr v-for="config in  configurations " :key="config.parameterKey">
 
-            <td v-for="(value, key) in config" :key="key">
-                <select v-if="chosenConfig === config.parameterKey && key === 'type'" v-model="config[key]">
+            <td v-for="(value, key) in  config " :key="key">
+                <select v-if="chosenConfig === config.parameterKey && !isDeleteButtonClicked && key === 'type'"
+                    v-model="config[key]">
                     <option value="int">int</option>
                     <option value="float">float</option>
                     <option value="string">string</option>
                 </select>
-                <input v-else-if="chosenConfig === config.parameterKey && key !== 'createDate' && key !== 'parameterKey'"
+                <input
+                    v-else-if="chosenConfig === config.parameterKey && !isDeleteButtonClicked && key !== 'createDate' && key !== 'parameterKey'"
                     v-model="config[key]" type="text" />
                 <span v-else>{{ value }}</span>
             </td>
@@ -16,7 +18,7 @@
             <td>
                 <div class="config-buttons">
                     <button v-if="chosenConfig === config.parameterKey && !isDeleteButtonClicked" class="button-done"
-                        @click="editConfig(config).then(chosenConfig = null)">Done</button>
+                        @click="editConfig(config).then(chosenConfig = null)">&#10003;</button>
                     <button v-else class="button-edit"
                         @click="chosenConfig = config.parameterKey; isDeleteButtonClicked = false">Edit</button>
                 </div>
@@ -25,7 +27,7 @@
             <td>
                 <div class="config-buttons">
                     <button v-if="chosenConfig === config.parameterKey && isDeleteButtonClicked" class="button-done"
-                        @click="deleteConfig(config).then(chosenConfig = null)">Done</button>
+                        @click="deleteConfig(config).then(chosenConfig = null)">&#10003;</button>
                     <button v-else class="button-delete"
                         @click="chosenConfig = config.parameterKey; isDeleteButtonClicked = true">
                         Delete</button>
